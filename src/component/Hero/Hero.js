8c6ce05f38259a1f6dfc9a060e67ff353468517f
Joshua Sung <yuniksung@gmail.com>
2021-06-25 01:59:19 -0500

    first commit

---
+++ src/component/Hero/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {  } from './HeroStyle'
+
+const Hero = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Hero
---
+++ src/component/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {  } from './NavbarStyle'
+
+const Navbar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Navbar
