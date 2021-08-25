85cc0f748e410c4c7982b4489351a9121f33f648
Joshua Sung <yuniksung@gmail.com>
2021-08-25 14:45:25 -0500

    selected box show and hide works on project

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -6,5 +6,19 @@ function boxToggle(){
     heroWrapper.classList.toggle('active');
 }
 
+// Show projects when selected
+function displayProjects(select){
+    if(select.value=='showAll'){
+        document.getElementById('frontEnds').style.display = "flex";
+        document.getElementById('backEnds').style.display = "flex";
+       } else if (select.value=='frontEnds'){
+        document.getElementById('frontEnds').style.display = "flex";
+        document.getElementById('backEnds').style.display = "none";
+       } else {
+        document.getElementById('frontEnds').style.display = "none";
+        document.getElementById('backEnds').style.display = "flex";
+       }
+}
+
 
 showMeBtn.addEventListener("click", boxToggle);
