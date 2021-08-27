1d4e38f84ac627be3096dfcdec4ec06421209f21
Joshua Sung <yuniksung@gmail.com>
2021-08-27 01:48:21 -0500

    building modals

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,4 +1,16 @@
 let showMeBtn = document.getElementById("showMe");
+const openEls = document.querySelectorAll("[data-open]");
+const isVisible = "is-visible";
+
+
+// Navigation display when scroll down
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("navbar").style.top = "0";
+    } else {
+      document.getElementById("navbar").style.top = "-50px";
+    }
+  }
 
 // Header, show my profile when clicked
 function boxToggle(){
@@ -8,17 +20,33 @@ function boxToggle(){
 
 // Show projects when selected
 function displayProjects(select){
-    if(select.value=='showAll'){
+    if(select.value =='showAll'){
+        console.log("show all")
         document.getElementById('frontEnds').style.display = "flex";
         document.getElementById('backEnds').style.display = "flex";
-       } else if (select.value=='frontEnds'){
+       } else if (select.value =='frontEnd'){
+        console.log("frontEnds")
         document.getElementById('frontEnds').style.display = "flex";
         document.getElementById('backEnds').style.display = "none";
-       } else {
+       } else if (select.value =='backEnd') {
+        console.log("backEnds")
         document.getElementById('frontEnds').style.display = "none";
+        document.getElementById('backEnds').style.display = "flex";
+       } else {
+        console.log("fullStacks")
+        document.getElementById('frontEnds').style.display = "flex";
         document.getElementById('backEnds').style.display = "flex";
        }
 }
 
 
+ 
+for(const el of openEls) {
+  el.addEventListener("click", function() {
+    const modalId = this.dataset.open;
+    document.getElementById(modalId).classList.add(isVisible);
+  });
+}
+
 showMeBtn.addEventListener("click", boxToggle);
+window.onscroll = function() {scrollFunction()};
